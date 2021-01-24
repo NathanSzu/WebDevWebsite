82ada2b698c4391f5e2f6f0cb93f091a1a9a5bcc
Nathan Szurek <nathan.szurek@yahoo.com>
2021-01-24 17:48:32 -0600

    Linked pages into App.js and created React router.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Nav />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/pricing' component={Pricing} />
+      <Route exact path='/portfolio' component={Portfolio} />
+      <Route exact path='/contact' component={Contact} />
+    </Router>
   );
 }
 
