ec773e3fd0acdbd47a5eb5161d6232b744161063
Nathan Szurek <nathan.szurek@yahoo.com>
2021-01-24 18:05:35 -0600

    Created basic nav element

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Nav() {
+
+    const location = useLocation();
+
     return (
-        <div>
-            
-        </div>
+        <nav className='row'>
+            <ul class='nav'>
+                <li class='nav-item'>
+                    <Link to='/' className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
+                </li>
+                <li class='nav-item'>
+                    <Link to='/pricing' className={location.pathname === '/pricing' ? 'nav-link active' : 'nav-link'}>Pricing</Link>
+                </li>
+                <li class='nav-item'>
+                    <Link to='/portfolio' className={location.pathname === '/portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Link>
+                </li>
+                <li class='nav-item'>
+                    <Link to='/contact' className={location.pathname === '/contact' ? 'nav-link active' : 'nav-link'}>Contact</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
